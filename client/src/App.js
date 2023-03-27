9d53736bd3f8efe81dfe08526f71a833495fb9da
nahom-getachew7 <garinahomgetachew@gmail.com>
2023-03-27 13:54:33 +0300

    login was added

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,6 @@ import {
 import Home from "./pages/home/Home";
 import Hotel from "./pages/hotel/Hotel";
 import List from "./pages/list/List";
-import Login from "./pages/login/Login";
 
 function App() {
   return (
@@ -15,8 +14,6 @@ function App() {
         <Route path="/" element={<Home/>}/>
         <Route path="/hotels" element={<List/>}/>
         <Route path="/hotels/:id" element={<Hotel/>}/>
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/login" element={<Login/>}/>
       </Routes>
     </BrowserRouter>
   );
